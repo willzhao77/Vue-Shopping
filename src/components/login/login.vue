f14bd609e0bb31f9cbfc629edd069457ad960b85
Will <willzhao77@gmail.com>
2020-03-01 21:02:35 +1300

    add login function.  success redirectly to personal page.

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -4,27 +4,62 @@
 
   <form class="form-signin">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="username" class="sr-only">User Name</label>
+      <input type="name" id="username" class="form-control" placeholder="username" required autofocus v-model="name">
       <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required v-model="password">
       <div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-block" type="submit" @click.prevent = login>Sign in</button>
       <button class="btn btn-lg btn-primary btn-block" type="submit" @click="goRegister">register</button>
     </form>
     </div>
 </template>
 
 <script>
+import { Toast } from 'mint-ui'
+
 export default {
+    data(){
+      return {
+        name:'',
+        password:'',
+
+      }
+    },
 
     methods:{
       goRegister(){
         this.$router.push('/register')
+      },
+
+      login(){
+        var formData = new FormData()
+            formData.append('name', this.name)
+            formData.append('password', this.password)
+         
+
+        this.$http.post('http://127.0.0.1:8000/api/login', formData)
+          .then(response => {
+                console.log(response.body);
+            // check user name and password and redirect to related page
+            if(response.body.api_token == '' || typeof response.body.api_token == 'undefined')
+            {
+              console.log("please Login again.");
+              // Toast('UserName or Password not correct')
+
+            }else{
+              // console.log("personal page");
+              this.$router.push('/person')
+            }
+
+            }, response => {
+              console.error(response.body);
+              Toast('UserName or Password not correct')
+            });
       }
     }
 }
