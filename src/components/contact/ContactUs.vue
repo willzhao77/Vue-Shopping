c4e34660bb924212368cf169c169082001408a18
Will <willzhao77@gmail.com>
2020-04-09 20:55:01 +1200

    user can send message from connect us

--- src/components/contact/ContactUs.vue
+++ src/components/contact/ContactUs.vue
@@ -4,21 +4,26 @@
             
             <div class="">
                 <label>Name:</label>
-                <input type="text" placeholder="" style="margin: 10px 0px;">
+                <input type="text" placeholder="" style="margin: 10px 0px;" name="name" v-model="name">
             </div>
 
             <div class="">
                 <label>Email:</label>
-                <input type="text" placeholder="" style="margin: 10px 0px;">
+                <input type="text" placeholder="" style="margin: 10px 0px;" name="email" v-model="email">
+            </div>
+
+            <div class="">
+                <label>Title:</label>
+                <input type="text" placeholder="" style="margin: 10px 0px;" name="title" v-model="title">
             </div>
 
             <label>Message:</label>
             <div class="mui-input-row" style="margin: 10px 0px;">
-                        <textarea id="textarea" rows="5" placeholder=""></textarea>
+                        <textarea id="textarea" rows="8" placeholder="" name="message" v-model="message"></textarea>
             </div>
 
             <div class="mui-button-row">
-                <button type="button" class="mui-btn mui-btn-primary" onclick="return false;">Send</button>&nbsp;&nbsp;
+                <button type="button" class="mui-btn mui-btn-primary" onclick="return false;" @click.prevent = SendMessage>Send</button>&nbsp;&nbsp;
                 <button type="button" class="mui-btn mui-btn-danger" onclick="return false;">Cancel</button>
             </div>
 		</form>
@@ -28,7 +33,42 @@
 </template>
 
 <script>
+import { Toast } from "mint-ui"
+export default {
+    data(){
+      return {
+        name:'',
+        email:'',
+        title:'',
+        message:'',
+      }
+    },
+    methods:{
+        SendMessage(){
+            var formData = new FormData()
+                formData.append('name', this.name)
+                formData.append('email', this.email)
+                formData.append('title', this.title)
+                formData.append('message', this.message)
 
+            this.$http.post('http://127.0.0.1:8000/api/contactus', formData)
+            .then(response => {
+                    if (response.bodyText === 'sent'){
+                        Toast('You succesfully sent message.')
+                        this.name = ''
+                        this.email=''
+                        this.title =''
+                        this.message =''
+                        
+                    }
+
+                }, response => {
+                console.error(response.body);
+                });
+        },
+    }
+
+}
 </script>
 
 
